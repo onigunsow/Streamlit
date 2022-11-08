78330da3fe64f660c24110f22365391f2bc211a1
onigunsow <49737700+onigunsow@users.noreply.github.com>
2022-11-08 19:33:44 +0900

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -23,9 +23,9 @@ st.set_page_config(
 
 
 # Data Prepare
-FilePath = "melting_tank_stream.csv"
-scaler_call = joblib.load("mx_rscaler.pkl") 
-model_call = load_model('melting_tank_pretrained_model.h5')
+FilePath = "/home/appuser/venv/bin/streamlit/melting_tank_stream.csv"
+scaler_call = joblib.load("/home/appuser/venv/bin/streamlit/mx_rscaler.pkl") 
+model_call = load_model('/home/appuser/venv/bin/streamlit/melting_tank_pretrained_model.h5')
 
 @st.experimental_memo
 def get_data() -> pd.DataFrame:
